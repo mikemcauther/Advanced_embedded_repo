234a26825a00c5c8a899d0af4bf3872a13d050b2
KO-CHEN CHI <k.chi@uqconnect.uq.au>
2020-04-04 10:18:13 +1000

    [buildenv] Fix small bug.

--- buildenv.sh
+++ buildenv.sh
@@ -4,15 +4,24 @@ set +x
 
 REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SOURCELIB_ROOT=$HOME/projects/csse4011/sourcelib
+
 TOOLCHAIN_PATH=/home/csse3010/projects/csse4011/tools
+TOOLCHAIN_DOWNLOAD_URL="https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-rm/downloads"
+
 EI_GIT_ADDRESS="https://github.com/uqembeddedsys/ei-freertos.git"
 EI_ROOT_PATH=${REPO_ROOT}/ei-freertos
+
 
 # Arm eabi check
 if [ -d ${TOOLCHAIN_PATH} ]; then
     echo ""
     echo "Try export PATH: ${TOOLCHAIN_PATH}/gcc-arm-none-eabi-7-2017-q4-major/bin"
     export PATH=${TOOLCHAIN_PATH}/gcc-arm-none-eabi-7-2017-q4-major/bin:${PATH}
+else
+    echo ""
+    echo "WARNING: We expect toolchain location in ${TOOLCHAIN_PATH}"
+    echo "You can try to download from ${TOOLCHAIN_DOWNLOAD_URL}"
+    echo "and install in ${TOOLCHAIN_PATH}"
 fi
 
 # Download EI-FREERTOS
@@ -24,5 +33,16 @@ if ! [ -d ${EI_ROOT_PATH} ]; then
 fi
 
 # Export EI related path
-echo ${PYTHONBPATH} | grep "${EI_ROOT_PATH}/pyclasses" || (export PYTHONPATH=${EI_ROOT_PATH}/pyclasses:$PYTHONPATH)
-echo ${PATH} | grep "${EI_ROOT_PATH}/tools" || (export PATH=${EI_ROOT_PATH}/tools:$PATH)
+echo ${PYTHONPATH} | grep "${EI_ROOT_PATH}/pyclasses" >/dev/null
+if [ $? -eq 1 ]; then
+    echo ""
+    echo "EXPORT: ${EI_ROOT_PATH}/pyclasses"
+    export PYTHONPATH=${EI_ROOT_PATH}/pyclasses:$PYTHONPATH
+fi
+
+echo ${PATH} | grep "${EI_ROOT_PATH}/tools" >/dev/null
+if [ $? -eq 1 ]; then
+    echo ""
+    echo "EXPORT: ${EI_ROOT_PATH}/tools"
+    export PATH=${EI_ROOT_PATH}/tools:$PATH
+fi
