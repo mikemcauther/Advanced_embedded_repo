a00c2be249a3ad8d18f1109cbd37d7705ba5b9a9
KO-CHEN CHI <k.chi@uqconnect.uq.au>
2020-04-04 21:25:26 +1000

    [buildenv] Fix buildenv.sh flow.

--- buildenv.sh
+++ buildenv.sh
@@ -2,23 +2,32 @@
 
 set +x
 
-REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJ_REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 TOOLCHAIN_PATH=/home/csse3010/projects/csse4011/tools
 TOOLCHAIN_DOWNLOAD_URL="https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-rm/downloads"
 
 EI_GIT_ADDRESS="https://github.com/uqembeddedsys/ei-freertos.git"
 
-export EI_ROOT_PATH=${REPO_ROOT}/ei-freertos
+EI_ROOT_PATH=${PROJ_REPO_ROOT}/ei-freertos
 
+# Variable to export in proj_common.mk
+#PROJ_PATH =
+#PROJ_PYTHONPATH =
+
+
+# Construct common config 
+cd ${PROJ_REPO_ROOT}
+rm -f ./common/proj_common.mk
+touch ./common/proj_common.mk
+echo "EI_ROOT_PATH=${PROJ_REPO_ROOT}/ei-freertos" >> ./common/proj_common.mk
 
 # Arm eabi check
 if [ -d ${TOOLCHAIN_PATH} ]; then
-    echo ${PATH} | grep "${TOOLCHAIN_PATH}/gcc-arm-none-eabi-7-2017-q4-major/bin" >/dev/null
-    if [ $? -eq 1 ]; then
+    if [ ${PATH} == ${PATH/${TOOLCHAIN_PATH}\/gcc-arm-none-eabi-7-2017-q4-major\/bin/} ]; then
         echo ""
-        echo "Try export PATH: ${TOOLCHAIN_PATH}/gcc-arm-none-eabi-7-2017-q4-major/bin"
-        export PATH=${TOOLCHAIN_PATH}/gcc-arm-none-eabi-7-2017-q4-major/bin:${PATH}
+        echo "Add PATH: ${TOOLCHAIN_PATH}/gcc-arm-none-eabi-7-2017-q4-major/bin"
+        PROJ_PATH=${TOOLCHAIN_PATH}/gcc-arm-none-eabi-7-2017-q4-major/bin${PROJ_PATH:+\:${PROJ_PATH}}
     fi
 else
     echo ""
@@ -31,21 +40,24 @@ fi
 if ! [ -d ${EI_ROOT_PATH} ]; then
     echo ""
     echo "Try download ei-freertos"
-    cd ${REPO_ROOT}
+    cd ${PROJ_REPO_ROOT}
     git clone ${EI_GIT_ADDRESS}
 fi
 
 # Export EI related path
-echo ${PYTHONPATH} | grep "${EI_ROOT_PATH}/pyclasses" >/dev/null
-if [ $? -eq 1 ]; then
+if [ ${PYTHONPATH} == ${PYTHONPATH/${EI_ROOT_PATH}\/pyclasses/} ]; then
     echo ""
-    echo "EXPORT: ${EI_ROOT_PATH}/pyclasses"
-    export PYTHONPATH=${EI_ROOT_PATH}/pyclasses:$PYTHONPATH
+    echo "Add PYTHONPATH: ${EI_ROOT_PATH}/pyclasses"
+    PROJ_PYTHONPATH=${EI_ROOT_PATH}/pyclasses${PROJ_PYTHONPATH:+\:${PROJ_PYTHONPATH}}
 fi
 
-echo ${PATH} | grep "${EI_ROOT_PATH}/tools" >/dev/null
-if [ $? -eq 1 ]; then
+if [ ${PATH} == ${PATH/${EI_ROOT_PATH}\/tools/} ]; then
     echo ""
-    echo "EXPORT: ${EI_ROOT_PATH}/tools"
-    export PATH=${EI_ROOT_PATH}/tools:$PATH
+    echo "Add PATH: ${EI_ROOT_PATH}/tools"
+    PROJ_PATH=${EI_ROOT_PATH}/tools${PROJ_PATH:+\:${PROJ_PATH}}
 fi
+
+# Print command into proj_common.mk
+echo "PATH=${PROJ_PATH}:${PATH}" >> ./common/proj_common.mk
+echo "PYTHONPATH=${PROJ_PYTHONPATH}${PYTHONPATH:+\:${PYTHONPATH}}" >> ./common/proj_common.mk
+echo "REPO_PATH=${EI_ROOT_PATH}" >> ./common/proj_common.mk
