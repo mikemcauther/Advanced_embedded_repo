8149c6fb322fea381527b1adddf83be3815225d6
KO-CHEN CHI <k.chi@uqconnect.uq.au>
2020-04-23 05:35:49 +1000

    [prac2][hci] Try to fix bug.

--- myoslib/scu/main.py
+++ myoslib/scu/main.py
@@ -8,8 +8,8 @@ glb_led_ctrl = led.LED_CTRL()
 #HCI
 glb_hci =   hci.HCI_UART()
 
-while True:
-    if glb_hci.any():
+while 1:
+    if glb_hci.any() != 0:
         glb_hci.startReceive()
 
     time.sleep(1)
