43498b92b7999fd77af0da26b575cacc6b2ab76f
KO-CHEN CHI <k.chi@uqconnect.uq.au>
2020-06-03 04:22:51 +1000

    [utility] update utility

--- overrite_diff_files_of_ei_changes.sh
+++ overrite_diff_files_of_ei_changes.sh
@@ -34,3 +34,35 @@ do
 done
 
 IFS=${OLD_IFS}
+
+
+
+
+
+FILES=$(cd "${EI_CHANGE_PATH}" && find ./* -type f)
+
+OLD_IFS=${IFS}
+IFS=$'\n'
+for PER_FILE in ${FILES}
+do
+    FILE_PATH="$( echo "${PER_FILE}" | sed  's/^[[:blank:]]*\.\/\([^\s]\)/\1/' )"
+    FILE_EI_ROOT_DIR_PATH=$(dirname "${EI_ROOT_PATH}/${FILE_PATH}")
+    FILE_EI_CHANGE_DIR_PATH=$(dirname "${EI_CHANGE_PATH}/${FILE_PATH}")
+    #echo "${FILE_PATH}"
+    echo ""
+    if [ -f "${EI_ROOT_PATH}/${FILE_PATH}" ]; then
+        if [ -f "${EI_CHANGE_PATH}/${FILE_PATH}" ]; then
+            TEMP_DIFF=$(diff ${EI_ROOT_PATH}/${FILE_PATH} ${EI_CHANGE_PATH}/${FILE_PATH})
+            if [ ${#TEMP_DIFF} -gt 0 ]; then
+                echo "Overrite FILE: ${EI_CHANGE_PATH}/${FILE_PATH}"
+                cp -f "${EI_ROOT_PATH}/${FILE_PATH}" "${EI_CHANGE_PATH}/${FILE_PATH}"
+            fi
+        else
+            echo "Not exist in CHANGE FOLDER: ${EI_CHANGE_PATH}/${FILE_PATH}"
+        fi
+    else
+        echo "Not exist in ROOT FOLDER: ${EI_ROOT_PATH}/${FILE_PATH}"
+    fi
+done
+
+IFS=${OLD_IFS}
